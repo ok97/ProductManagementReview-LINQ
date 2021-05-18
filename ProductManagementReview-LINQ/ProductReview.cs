e667a61c8512af6c2c9df1804b20cd95eb68cc7e
ok97 <omkhawshi0@gmail.com>
2021-05-18 23:52:34 +0530

    Added: Creating List Of Product Review

---
+++ ProductManagementReview-LINQ/ProductReview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementReview_LINQ
+{/* UC1:- Product Review Management
+              - Create Product review class with ProductID, UserID, Rating, Review and isLike fields.
+              - Create variable for List of Product Review class in Main method
+              - Add 25 default values in list which we have created.
+        */
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
